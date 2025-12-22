b19cf3ae0c7d5509015590aa0c74e2fafc292ab5
Prabin Yadav <prabin.yadav935@gmail.com>
2025-12-22 22:59:59 +0530

    Add files via upload

---
+++ LAB_1/Elseif.java
@@ -0,0 +1,15 @@
+public class Elseif {
+    public static void main(String[] args) {
+        int marks = 82;
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        }
+        else if (marks >= 75) {
+            System.out.println("Grade B");
+        }
+        else
+        {
+            System.out.println("Grade C");
+        }
+    }
+}
