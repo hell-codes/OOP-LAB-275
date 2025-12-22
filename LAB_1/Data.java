c90e39b6f7fb6476c26bba7af829fdb0e9305d40
Prabin Yadav <prabin.yadav935@gmail.com>
2025-12-22 22:44:05 +0530

    Add files via upload

---
+++ LAB_1/Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public static void main(String[] args) {
+        int age = 20;
+        double marks = 88.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Devil";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
