c8ddec51c3d024c1601c49e4abbe8650878b33e1
Prabin Yadav <prabin.yadav935@gmail.com>
2025-12-29 18:56:23 +0530

    Add files via upload

--- LAB_1/Operations.java
+++ LAB_1/Operations.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
+
 public class Operations {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter first num:");
-        int a = input.nextInt();
+        double a = input.nextDouble();
         
         System.out.println("Enter second num:");
-        int b = input.nextInt();
+        double b = input.nextDouble();
 
+        // Arithmetic Operators
+        System.out.println("\n--- Arithmetic Operators ---");
         System.out.println("Add: " +(a+b));
         System.out.println("Sub: " +(a-b));
         System.out.println("Mul: " +(a*b));
         System.out.println("Div: " +(a/b));
         System.out.println("Mod: " +(a%b));
-       
+
+       // Increment & Decrement
+        System.out.println("\n--- Increment / Decrement ---");
         System.out.println("Increment(a++): " +(a++)); //use value then add 1
         System.out.println("Decrement(a--): " +(a--)); //use value then minus 1
         System.out.println("Increment(++a): " +(++a)); //add 1 then use value
@@ -25,6 +30,20 @@ public class Operations {
         System.out.println("Increment(++b): " +(++b)); //add 1 then use value
         System.out.println("Increment(--b): " +(--b)); //minus 1 then use value
 
+        // Relational Operators
+        System.out.println("\n--- Relational Operators ---");
+        System.out.println("a == b: " +(a == b));
+        System.out.println("a != b: " +(a != b));
+        System.out.println("a > b: " +(a > b));
+        System.out.println("a < b: " +(a < b));
+        System.out.println("a >= b: " +(a >= b));
+        System.out.println("a <= b: " +(a <= b));
+
+        // Logical Operators
+        System.out.println("\n--- Logical Operators ---");
+        System.out.println("(a > b) && (a != b): " + ((a > b) && (a != b)));
+        System.out.println("(a > b) || (a == b): " + ((a > b) || (a == b)));
+        System.out.println("!(a == b): " + (!(a == b)));
 
         input.close();
     }
