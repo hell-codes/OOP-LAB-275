3a3f0e1341daba034e5ccd05de18cdb6f353608b
Prabin Yadav <prabin.yadav935@gmail.com>
2026-04-01 20:31:34 +0530

    Add files via upload

---
+++ Abstraction/access-modifiers/Default.java
@@ -0,0 +1,9 @@
+class Demo {
+    int value = 50;
+}
+public class Default {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println(d.value);
+    }
+}
