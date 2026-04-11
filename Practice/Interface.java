1a7c509d099886f681dc6f0ac7bc508f6555de69
Prabin Yadav <prabin.yadav935@gmail.com>
2026-04-12 03:08:22 +0530

    Add files via upload

---
+++ Practice/Interface.java
@@ -0,0 +1,24 @@
+package Practice;
+
+interface Vehicle {
+    void start();
+    void stop();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car started");
+    }
+
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.start();
+        c.stop();
+    }
+}
