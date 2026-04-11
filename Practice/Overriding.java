6a8ea434e4e2545fdf716eb46bb427fbc433e3f1
Prabin Yadav <prabin.yadav935@gmail.com>
2026-04-12 02:43:29 +0530

    Add files via upload

---
+++ Practice/Overriding.java
@@ -0,0 +1,33 @@
+package Practice;
+
+class Shape {
+    void area() {
+        System.out.println("Calculating area...");
+    }
+}
+
+class Circle extends Shape {
+    void area() {
+        double r = 5;
+        System.out.println("Circle Area: " + (3.14 * r * r));
+    }
+}
+
+class Rectangle extends Shape {
+    void area() {
+        int l = 4, b = 6;
+        System.out.println("Rectangle Area: " + (l * b));
+    }
+}
+
+public class Overriding {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.area();
+
+        s = new Rectangle();
+        s.area();
+    }
+}
